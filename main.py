933400804355863637f1937af368cf14883844f9
HugoLennartsson <hugo.lennartsson@outlook.com>
2026-01-26 21:54:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from view.window import run 
+
+if __name__ == "__main__":
+    run()
---
+++ view/window.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+def run():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("Algorithm Visualizer")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+        screen.fill((30,30,30))
+
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
